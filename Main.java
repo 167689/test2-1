493711583da8cc793219f740bd96bf6bf8615152
JCwynar <101180601+167689@users.noreply.github.com>
2022-04-05 05:56:51 +0000

    1,0

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
   public static void main(String[] args) {
-    int z,x,c;
+    int z,x,c=2;
     for(z=0;z<9;z++)
     {
       for(x=0;x<9*2;x++)
@@ -16,6 +16,20 @@ class Main {
       }
        System.out.println();
     }
-    
+    for(z=0;z<3;z++)
+    {
+      for(x=0;x<9*2;x++)
+      {
+      if(x<(9-c)||x>(9+c)) 
+      {
+      System.out.print(" "); 
+      }
+        else 
+      {
+        System.out.print("*");
+      }
+      }
+       System.out.println();
+    }
   }
 }
