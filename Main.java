55fea6a21cf3122aa080f0e0e91fe9a4c8c72bcf
JCwynar <101180601+167689@users.noreply.github.com>
2022-04-05 06:57:08 +0000

    2,0

--- Main.java
+++ Main.java
@@ -1,35 +1,25 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    int z,x,c=2;
-    for(z=0;z<9;z++)
-    {
-      for(x=0;x<9*2;x++)
-      {
-      if(x<(9-z)||x>(9+z)) 
-      {
-      System.out.print(" "); 
-      }
-        else 
-      {
-        System.out.print("*");
-      }
-      }
-       System.out.println();
-    }
-    for(z=0;z<3;z++)
-    {
-      for(x=0;x<9*2;x++)
-      {
-      if(x<(9-c)||x>(9+c)) 
-      {
-      System.out.print(" "); 
-      }
-        else 
-      {
-        System.out.print("*");
-      }
-      }
-       System.out.println();
-    }
+    int v,b;
+     Scanner z = new Scanner(System.in);
+        System.out.println("Podaj tekst z zamianą na duże");
+        String x = z.next();
+        System.out.println("Podaj tekst z zamianą na małe ");
+        String c=z.next();
+        for(v=0;v<x.length();v++)
+        {
+        x=(x.toUpperCase());
+          }
+    for(b=0;b<c.length();b++)
+        {
+        c=(c.toLowerCase());
+          }
+        System.out.println(x);
+    System.out.println(c);
+    System.out.println(v);
+    System.out.println(b);
+
+        z.close();
   }
 }
